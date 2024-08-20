43654683de29eec3ddeff6e33da57d3b05148d6f
Nyelton de Castro <nyeltoncastro@gmail.com>
2024-08-20 10:39:05 -0300

    Primeira aula sobre listas

---
+++ tipos_variaveis/lista.py
@@ -0,0 +1,19 @@
+# Declaração
+
+minha_lista = [1, 2, 3, 4, 5, "rocketseat", True, False]
+
+# Exibindo a lista toda
+print("Minha lista de exemplo: ", minha_lista)
+
+# Exibindo um elemento da lista
+print("Exibindo o primeiro elemento da minha lista:",minha_lista[0])
+
+# Fatiar/Slice Exibindo do elemento x ao y
+print("Exibindo do 1 ao 5:", minha_lista[0:5])
+
+# Fatiando do começo ao xº elemento
+print("Exibindo do começo a o rocketseat:",minha_lista[:6])
+
+# Fatiando do xº elemento ao final 
+print("Exibindo de rocketseat ao final:",minha_lista[5:])
+
