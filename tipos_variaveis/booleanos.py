b4d3f77cc8dd850fbebbce5784b2839c5a6a4a38
Nyelton de Castro <nyeltoncastro@gmail.com>
2024-08-20 10:18:41 -0300

    Aula de Booleanos e introdução a operadores lógicos

---
+++ tipos_variaveis/booleanos.py
@@ -0,0 +1,32 @@
+# Condição verdadeira
+True
+# Condição falsa
+False
+
+# Condicionas
+# If = se condição
+# Else = se não
+if True:
+    print("Bloco if vai ser executado")
+else:
+    print("Bloco ELSE não será executado")
+
+# Operadores lógicos : and e or
+
+# And
+if True and True:
+    print("Bloco será executado")
+    
+if True and False:
+    print("Bloco não sera executado")
+
+if False and False:
+    print("Bloco não será executado")  
+
+# Or
+if True or False:
+    print("Bloco OR vai ser executado")
+if False or False:
+    print("Bloco OR não será executado")
+if True or True:
+    print("Bloco OR será executado")    
